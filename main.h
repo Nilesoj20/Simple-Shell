5067d22f62d925589e653b78e7cfce655a3b2af9
Bryan Yep <yepsan95@gmail.com>
2023-04-20 21:57:48 -0700

    Added header file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+char **_strtok(char *str);
+int count_words(char *str);
+
+#endif /*MAIN_H*/
