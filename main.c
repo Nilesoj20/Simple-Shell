0e6202292355a2a98d48b300efe9e1e2fbed6e0f
diegosmen111 <dbav_10@hotmail.com>
2023-04-24 13:32:07 -0700

    updates

--- main.c
+++ main.c
@@ -3,29 +3,36 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+
 int main(void)
 {
     char *array[9999];
     char *line = NULL;
-    int i, words_n;
     size_t len = 0;
     ssize_t read;
+    
     printf("$ ");
     while ((read = getline(&line, &len, stdin)) != -1)
     {
-        words_n = count_words(line);
+        int words_n = count_words(line);
         char *temp = strtok(line, " \n");
-    for (i = 0; i < words_n && temp != NULL; i++)
-    {
-        array[i] = temp;
-        temp = strtok(NULL, " \n");
-    }
+
+        int i;
+        for (i = 0; i < words_n && temp != NULL; i++)
+        {
+            array[i] = temp;
+            temp = strtok(NULL, " \n");
+        }
         array[i] = NULL;
-    if (execve(array[0], &array[0], NULL) == -1)
-        perror("Error:");
+        
+        if (execve(array[0], &array[0], NULL) == -1)
+            perror("Error:");
+        
         printf("$ ");
-    if (line)
-        free(line);
+        
+        if (line)
+            free(line);
     }
+    
     return (0);
 }
