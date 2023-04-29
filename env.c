7ec981f65f7c17c6daf2e6efca949dadc7bdc837
Bryan Yep <yepsan95@gmail.com>
2023-04-29 15:37:18 -0700

    fix: fixed env built-in

--- env.c
+++ env.c
@@ -7,47 +7,8 @@
  */
 void print_env(void)
 {
-	pid_t ppid;
-	int ppid_fd, file_size, n;
-	char *ppid_str, ppid_path[20] = "/proc/";
-	struct stat st;
-	char buf[1024];
+	int i;
 
-	ppid = getpid();
-	ppid_str = _itoa(ppid);
-	_strcat(ppid_path, ppid_str);
-	_strcat(ppid_path, "/environ");
-	if (stat(ppid_path, &st) == -1)
-	{
-		if (ppid_str != NULL)
-			free(ppid_str);
-		perror("Failed stat function");
-		exit(1);
-	}
-	file_size = st.st_size;
-
-	ppid_fd = open(ppid_path, O_RDONLY);
-	if (ppid_fd == -1)
-	{
-		free(ppid_str);
-		perror("Failed open function.");
-		exit(1);
-	}
-
-	while ((n = read(ppid_fd, buf, sizeof(buf))) > 0)
-	{
-		file_size = file_size + n;
-		if (write(STDOUT_FILENO, buf, n) != n)
-		{
-			free(ppid_str);
-			perror("Failed write function");
-			exit(1);
-		}
-	}
-	free(ppid_str);
-	if (close(ppid_fd) == -1)
-	{
-		perror("Failed close function");
-		exit(1);
-	}
+	for (i = 0; environ[i] != NULL; i++)
+		printf("%s\n", environ[i]);
 }
