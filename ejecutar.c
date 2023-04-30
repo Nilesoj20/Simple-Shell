322abebf63ed250d568c196676e923968928c8c4
Nilesoj20 <jvrn2014@gmail.com>
2023-04-30 09:53:27 -0700

    fix: leaving everything the same

--- ejecutar.c
+++ ejecutar.c
@@ -8,42 +8,47 @@
  */
 int ejecutar(char **av)
 {
-	pid_t pid;
-	int status;
-	struct stat buffer;
-	char *comando = NULL;
+        pid_t pid;
+        int status;
+        char *comando = NULL, *actual_comando = NULL;
 
-	comando = av[0];
-	if (stat(comando, &buffer) != 0)
-	{
-		printf("Error: comando incorrecto stat, linea 18, ejecutar\n");
-		return (0);
-	}
-	if (comando == NULL)
-		printf("Error: comando incorrecto\n");
-	else
-	{
-		/*creo un proceso hijo*/
-		pid = fork();
-		/* valido que salio bien */
-		if (pid == -1)
-		{
-			perror("Error: fork");
-			exit(2);
-		}
-		/* valido que fue exito*/
-		if (pid == 0)
-		{
-			/*ejacutamos el programa y validamos si fallo*/
-			if (execve(comando, av, environ) == -1)
-				perror("Error: execve");
-		}
-		else
-		{
-			/* status se encarga de ver si el hijo termino correctamente*/
-			wait(&status);
-			return (0);
-		}
-	}
-	return (0);
+        comando = av[0];
+        /* busca la ruta path antes de pasarlo a execve*/
+        actual_comando = buscar_ruta(comando);
+        if (actual_comando == NULL)
+        {
+                printf("Error: comando incorrecto\n");
+                return (0);
+        }
+        else
+        {
+                /*creo un proceso hijo*/
+                pid = fork();
+                /* valido que salio bien */
+                if (pid == -1)
+                {
+                        if (actual_comando != NULL)
+                                free(actual_comando);
+                        perror("Error: fork");
+                        return (0);
+                }
+                /* valido que fue exito*/
+                if (pid == 0)
+                {
+                        /*ejacutamos el programa y validamos si fallo*/
+                        if (execve(actual_comando, av, environ) == -1)
+                                perror("Error: execve");
+                        if (actual_comando != NULL)
+                                free(actual_comando);
+                }
+                else
+                {
+                        /* status se encarga de ver si el hijo termino correctamente*/
+                        wait(&status);
+                        if (actual_comando != NULL)
+                                free(actual_comando);
+                        return (0);
+                }
+        }
+        return (0);
 }
